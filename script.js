70ff2f10067ad0e58be0822ee5d415035794955e
sezasada <111367099+sezasada@users.noreply.github.com>
2023-01-07 15:11:30 -0800

    update html and js
    created a click handler and a function that grabs values from the dom and creates a new object

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 console.log('in js');
+
+$(function () {
+    //setting up the click handlers.
+    $('#submit-button').on('click', addEmployee);
+});
+
+function addEmployee() {
+
+    // creating an object for employees
+    const employeeObject = {
+        firstName: $('#first-name').val(),
+        lastName: $('#last-name').val(),
+        id: $('#id').val(),
+        title: $('#title').val(),
+        annualSalary: $('#annualSalary').val()
+    };
+}
